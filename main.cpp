8f89ded18af8e482ae40084a1993479c41186bee
Welpike <pascal.lleonart@gmail.com>
2023-04-23 23:05:05 +0200

    vars

--- main.cpp
+++ main.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
+#include <string>
+
+// tutorial (in french) : https://www.youtube.com/playlist?list=PLrSOXFDHBtfFKOzlm5iCBeXDTLxXdmxpx
 
 int main()
 {
-    std::cout << "Hello, world!" << std::endl;
-    std::cerr << "error" << std::endl;
+    //std::cout << "Hello, world!" << std::endl;
+    //std::cerr << "error" << std::endl;
+
+    /*
+
+    bool is_secure{};
+
+    std::cout << is_secure << std::endl;  // -> 0
+    std::cout << !is_secure << std::endl;  // -> 1
+
+    //int number{42};
+    float number{3.14};
+
+    long long big_number = 14'850'000;
+
+    std::cout << number << std::endl;*/
+
+    char c{'A'};
+
+    std::string welcome_msg{"Hello, World!"};
+
+    std::cout << welcome_msg << std::endl;
+
     return 0;
 }
