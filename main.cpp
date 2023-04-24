0c4db336c804760d19cb467bd9840eaad889711d
Welpike <pascal.lleonart@gmail.com>
2023-04-24 15:47:11 +0200

    inputs

--- main.cpp
+++ main.cpp
@@ -40,6 +40,32 @@ int main()
 
     auto my_data_str{"Hello"s};  // type of my_data_str = std::string (and not default from C)*/
 
+    // for inputs with no blanks
+    /*std::cout << "Enter your first name : ";
+
+    std::string user_firstname;
+    std::cin >> user_firstname;
+
+    std::cout << "Your first name : " << user_firstname << std::endl;*/
+
+    // for inputs with blanks
+    
+    /*std::cout << "Enter your first and last names : ";
+
+    std::string user_name;
+    std::getline(std::cin, user_name);  // can get a 3rd arg = delimiter (default="\n")
+
+    std::cout << "Your name : " << user_name << std::endl;*/
+
+    /*std::cout << " Good : " << std::cin.good() << std::endl;
+    std::cout << " Fail : " << std::cin.fail() << std::endl;
+    std::cout << " Bad : " << std::cin.bad() << std::endl;*/
+
+    // for ignore the chars that are in the 'cache' between 2 inputs
+    // std:cin.ignore(255, '\n');
+
+    
+
 
     return 0;
 }
